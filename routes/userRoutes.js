ef3b0cf5c2bf14c95dc8ebc0ee0d82009c99c415
Kunal K V <kunalvijayakumar.021200@gmail.com>
2025-05-05 13:06:12 +0530

    Delete routes directory

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const { getMe } = require('../controllers/userController');
-
-router.get('/me', auth, getMe);
-
-module.exports = router;
